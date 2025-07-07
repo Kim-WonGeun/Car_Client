cecd2cb0fadd50f81f07a68d2e57094e96d2d960
wg <wgkim9310@gmail.com>
2025-07-07 17:31:42 +0900

    - AddCar.tsx update

---
+++ src/components/AddCar.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addCar } from "../api/carapi";
+import CarDialogContent from "./CarDialogContent";
+import type { Car } from '../types';
+
+function AddCar() {
+
+    const queryClient = useQueryClient();
+
+    const { mutate } = useMutation(addCar, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["cars"]);
+        },
+        onError: (err) => {
+            console.log(err);
+        },
+    });
+
+    const [open, setOpen] = useState(false);
+    const [car, setCar] = useState<Car>({
+        brand: '',
+        model: '',
+        color: '',
+        registrationNumber: '',
+        modelYear: 0,
+        price: 0
+    });
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+        setCar({...car, [event.target.name]:
+            event.target.value});
+    }
+
+    const handleSave = () => {
+        mutate(car);
+        setCar({
+            brand: '',
+            model: '',
+            color: '',
+            registrationNumber: '',
+            modelYear: 0,
+            price: 0
+        });
+        handleClose();
+    }
+
+    return (
+        <>
+            <button onClick={handleClickOpen}>New Car</button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>New Car</DialogTitle>
+                <CarDialogContent car={car} handleChange={handleChange}/>
+                <DialogActions>
+                    <button onClick={handleClose}>Cancel</button>
+                    <button onClick={handleSave}>Save</button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
+
+export default AddCar;
